93c171ad2bf5196ddee968ef3692b068934a7f36
Marina <schwmk@gmail.com>
2018-02-23 08:08:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
-class Sorter { 
-	constructor() { 
-		this.arrayOfElements = [];
-		this.compareFunction = (right, left) => right - left;
-	}
-	
-	add(element) {
-		this.arrayOfElements.push(element);
-	}
-	
-	at(index) {
-		return this.arrayOfElements[index];
-	}
-	
-	get length() {
-		return this.arrayOfElements.length;
-	}
-	
-	toArray() {
-		return this.arrayOfElements;
-	}
-	
-	sort(){
-		indices.sort(this.compareFunction);
-    
-		var array = [];
-		for (let i = 0; i < indices.length; i++) {
-			array.push(this.arrayOfElemnts[indices[i]]);
-		}
-			array.sort(this.compareFunction);
-    
-		for (let i = 0; i < array.length; i++) {
-			this.arrayOfElemnts[indices[i]] = array[i];
-		}
-	}
-}
- /* setComparator(compareFunction) {
-    // your implementation
-  }*/
+class Sorter {
+  constructor() {
+    this.arrayOfElemnts = [];
+    this.compareFunction = (right, left) => right - left;
+  }
 
+  add(element) {
+    this.arrayOfElemnts.push(element);
+  }
+
+  at(index) {
+    return this.arrayOfElemnts[index];
+  }
+
+  get length() {
+    return this.arrayOfElemnts.length;
+  }
+
+  toArray() {
+    return this.arrayOfElemnts;
+}
+
+  sort(indices) {
+    indices.sort(this.compareFunction);
+    
+    var array = [];
+    for (let i = 0; i < indices.length; i++) {
+      array.push(this.arrayOfElemnts[indices[i]]);
+    }
+    array.sort(this.compareFunction);
+    
+    for (let i = 0; i < array.length; i++) {
+      this.arrayOfElemnts[indices[i]] = array[i];
+    }
+  }
+}
 module.exports = Sorter;
